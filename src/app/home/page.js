5fb4a245f77ff462ed87410b46ba34792b35e689
ESMENIELD <esmeralda.nield@alu.ing.unlp.edu.ar>
2025-09-03 15:05:11 -0300

    mala importacion en  home

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -5,7 +5,7 @@ import EntryForm from '../components/EntryForm';
 import EntryList from '../components/EntryList';
 import DownloadPDFButton from '../components/DownloadPDFButton';
 import { useAuth } from '../Login/context/AuthContext';
-import useApi from '../Login/hooks/useApi';
+import {useApi} from '../Login/hooks/useApi';
 
 export default function Home() {
   const { user, loading } = useAuth();
